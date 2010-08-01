2b9b110268189f439166d2b05ca2d68c3d05df2f
Rintaro Ishizaki <rintaro@lab.alfredcore.com>
2010-08-02 00:18:58 +0900

    initial commit

---
+++ lib/Net/IP/AddrRanges.pm
@@ -0,0 +1,32 @@
+package Net::IP::AddrRanges;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::IP::AddrRanges -
+
+=head1 SYNOPSIS
+
+  use Net::IP::AddrRanges;
+
+=head1 DESCRIPTION
+
+Net::IP::AddrRanges is
+
+=head1 AUTHOR
+
+Rintaro Ishizaki E<lt>rintaro@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
